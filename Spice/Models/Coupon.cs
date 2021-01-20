a7e5bbcd32ed00cae72785c9b6e9878a919c9180
Margolp80 <margolisyehuda@gmail.com>
2021-01-20 14:30:38 +0200

    first commit - add all files to repo

---
+++ Spice/Models/Coupon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class Coupon
+    {
+        public enum ECouponType { Percent = 0, Dollar = 1 }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string CouponType { get; set; }
+
+
+
+        [Required]
+        public double Discount { get; set; }
+
+        [Required]
+        public double MinimumAmount { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        [Display(Name = "Is Active")]
+        public bool IsActive { get; set; }
+    }
+}
